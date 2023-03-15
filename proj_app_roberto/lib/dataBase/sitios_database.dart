b7577b88c6c4b44fbd3165c70c424037461a2d5d
MurgaRoberto <robertomurga01@gmail.com>
2023-03-15 14:27:34 +0100

    comienzo bbdd

---
+++ proj_app_roberto/lib/dataBase/sitios_database.dart
@@ -0,0 +1,12 @@
+class Sitios {
+  final int id;
+  final String nombre;
+  final int direccion;
+  final int descripcion;
+  final int img;
+  final int latitud;
+  final int longitud;
+
+  Sitios({required this.id, required this.nombre, required this.direccion, required this.descripcion, required this.img, required this.latitud, required this.longitud});
+
+}
--- proj_app_roberto/lib/theme/app_theme.dart
+++ proj_app_roberto/lib/theme/app_theme.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Color.fromARGB(255, 255, 188, 89);
+  static const Color primary = Color.fromARGB(255, 170, 170, 170);
   static const Color iconsColor = Color.fromARGB(255, 170, 170, 170);
 
   static final mainTheme = ThemeData.light().copyWith(
 
       //AppBarTheme
       appBarTheme: const AppBarTheme(
-          color: primary, elevation: 0, foregroundColor: Colors.white),
+          centerTitle: true,
+          color: primary, elevation: 0, foregroundColor: Colors.white),          
 
       //ListTile
       listTileTheme: const ListTileThemeData(
